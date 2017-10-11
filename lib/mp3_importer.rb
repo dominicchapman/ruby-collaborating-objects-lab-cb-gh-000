8c849a026fc992622ab74933d5fb0b611b7e16d7
Dominic Chapman <dominicchapman@me.com>
2017-10-11 10:36:30 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,6 +1,15 @@
 
 class MP3Importer
 
+  attr_accessor :path
 
+  def initialize(filepath)
+    @path = filepath
+  end
 
 end
+
+# test_music_path = "./spec/fixtures/mp3s"
+# music_importer = MP3Importer.new(test_music_path)
+# music_importer.path
+#=> test_music_path
