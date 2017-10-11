ca696d789f8c2941a0c8b8eb0688f99a5d67cfc2
Dominic Chapman <dominicchapman@me.com>
2017-10-11 10:18:10 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,14 +3,26 @@
 # artist.name
 #=> 'Michael Jackson'
 
+# song_one = Song.new("Rock With You")
+# song_two = Song.new("Smooth Criminal")
+# artist.add_song(song_one)
+# artist.add_song(song_two)
+# expect(artist.songs).to eq([song_one, song_two])
+
 class Artist
 
   # Artist #name= sets the artist name
-  attr_accessor :name
+  # Artist #songs keeps track of an artist's songs
+  attr_accessor :name, :songs
 
   # Artist #initialize with #name accepts a name for the artist
   def initialize(name)
     @name = name
+    @songs = []
+  end
+
+  def add_song(song)
+    @songs << song
   end
 
 end
