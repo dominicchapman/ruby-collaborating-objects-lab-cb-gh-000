22a8c4187e49ec0202165d7bfb2674e7fe415e63
Dominic Chapman <dominicchapman@me.com>
2017-10-11 09:51:02 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,4 +1,20 @@
 
+# song = Song.new('Man in the Mirror')
+# file_name = 'Michael Jackson - Black or White - pop.mp3'
+
+# song.name
+#=> 'Man in the Mirror'
+
+
+
 class Song
+
+  # Song #name= sets the song name
+  attr_accessor :name
+
+  # Song #initialize with #name accepts a name for the song
+  def initialize(name)
+    @name = name
+  end
 
 end
